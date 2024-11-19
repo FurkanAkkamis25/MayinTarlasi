b86983df88d8490d427b1f62a87eccb8b1da8a5c
FurkanAkkamis25 <147555571+FurkanAkkamis25@users.noreply.github.com>
2024-11-19 15:20:22 +0300

    Initial Commit

---
+++ MayinTarlasi/MayinTarlasi/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace MayinTarlasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(-1, -2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(693, 61);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mayın Tarlası";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(17, 65);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(515, 574);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.button1.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ImageKey = "(none)";
+            this.button1.Location = new System.Drawing.Point(538, 102);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 32);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Başla";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.button2.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ImageKey = "(none)";
+            this.button2.Location = new System.Drawing.Point(538, 140);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(89, 30);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Skor:";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(633, 146);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(47, 21);
+            this.textBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.Appearance.BackColor = System.Drawing.Color.Silver;
+            this.Appearance.Options.UseBackColor = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(692, 651);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ MayinTarlasi/MayinTarlasi/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MayinTarlasi
+{
+    public partial class Form1 : DevExpress.XtraEditors.XtraForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int skor = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MayinDoldur(20, 20);
+        }
+        void MayinDoldur(int mayin,int boyut)
+        {
+            flowLayoutPanel1.Controls.Clear();
+            int[] mayinlar = new int[mayin];
+            Random rnd = new Random();
+            for(int i=0;i< 20; i++)
+            {
+               int secilen = rnd.Next(0, boyut * boyut);
+                if (mayinlar.Contains(secilen))
+                {
+                    i--;
+                    continue;
+                }
+                mayinlar[i] = secilen;
+            }
+
+            for(int i = 0; i < boyut * boyut; i++)
+            {
+                Button btn = new Button();
+                btn.Width = 25;
+                btn.Height = 25;
+                btn.Margin = new Padding(0);
+                btn.Tag = mayinlar.Contains(i);
+                btn.Click += btn_click;
+                flowLayoutPanel1.Controls.Add(btn);
+            }
+        }
+        void btn_click(object sender,EventArgs e)
+        {
+            Button tiklanan = (Button)sender;
+            bool durum = (bool)tiklanan.Tag;
+            if (durum == true)
+            {
+                tiklanan.BackColor = Color.Red;
+                oyunBitir(); 
+            }
+            else
+            {
+                tiklanan.BackColor = Color.Green;
+                skor++;
+                textBox1.Text = skor.ToString();
+            }
+
+        }
+        void oyunBitir()
+        {
+            skor = 0;
+            foreach(Button item in flowLayoutPanel1.Controls)
+            {
+                bool durum = (bool)item.Tag;
+                if (durum)
+                {
+                    item.BackColor = Color.Red;
+                }
+                else
+                {
+                    item.BackColor = Color.Green;
+                }
+            }
+          DialogResult sonuc=  MessageBox.Show("Oyun Bitti :( Yeniden Oynamak İster Misiniz ?","İster Misiniz ?",MessageBoxButtons.YesNo);
+            if (sonuc == DialogResult.Yes)
+                MayinDoldur(20, 20);
+        }
+      
+    }
+}
